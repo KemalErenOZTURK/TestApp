e92d3c6e06cf6b7ebe07e50ac8e70eb928f82eb6
Kemal <kemalerenozturk@gmail.com>
2023-04-22 17:02:50 +0300

    X-Real-IP

--- TestApp/Controllers/HomeController.cs
+++ TestApp/Controllers/HomeController.cs
@@ -14,42 +14,44 @@ namespace TestApp.Controllers
         }
         public IActionResult Index()
         {
-            string _ipAddress = string.Empty;
-            IPAddress? remoteIpAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress;
-            if (remoteIpAddress != null && remoteIpAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                _ipAddress = remoteIpAddress.ToString();
-            else
-            {
-                if (remoteIpAddress != null && remoteIpAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
-                {
-                    _ipAddress = System.Net.Dns.GetHostEntry(remoteIpAddress).AddressList.First(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
-                }
-                else
-                {
-                    var xForwardedForHeader = _httpContextAccessor.HttpContext.Request.Headers["X-Forwarded-For"].ToString();
+            var ip = _httpContextAccessor.HttpContext.Request.Headers["X-Real-IP"].ToString();
 
-                    if (!string.IsNullOrEmpty(xForwardedForHeader))
-                    {
-                        var ipAddresses = xForwardedForHeader.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            //string _ipAddress = string.Empty;
+            //IPAddress? remoteIpAddress = _httpContextAccessor.HttpContext.Connection.RemoteIpAddress;
+            //if (remoteIpAddress != null && remoteIpAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            //    _ipAddress = remoteIpAddress.ToString();
+            //else
+            //{
+            //    if (remoteIpAddress != null && remoteIpAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6)
+            //    {
+            //        _ipAddress = System.Net.Dns.GetHostEntry(remoteIpAddress).AddressList.First(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
+            //    }
+            //    else
+            //    {
+            //        var xForwardedForHeader = _httpContextAccessor.HttpContext.Request.Headers["X-Forwarded-For"].ToString();
 
-                        if (ipAddresses.Length > 0)
-                        {
-                            foreach (var ipAddress in ipAddresses)
-                            {
-                                if (IPAddress.TryParse(ipAddress.Trim(), out remoteIpAddress))
-                                {
-                                    if (remoteIpAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                                    {
-                                        _ipAddress = remoteIpAddress.ToString();
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            return Json(_ipAddress);
+            //        if (!string.IsNullOrEmpty(xForwardedForHeader))
+            //        {
+            //            var ipAddresses = xForwardedForHeader.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //            if (ipAddresses.Length > 0)
+            //            {
+            //                foreach (var ipAddress in ipAddresses)
+            //                {
+            //                    if (IPAddress.TryParse(ipAddress.Trim(), out remoteIpAddress))
+            //                    {
+            //                        if (remoteIpAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            //                        {
+            //                            _ipAddress = remoteIpAddress.ToString();
+            //                            break;
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+            return Json(ip);
         }
     }
 }
